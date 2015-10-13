2ed690f151a364b98bd73b51dfa062d9694d6e37
Nelson DA COSTA <ndacosta@octo.com>
2015-10-13 11:53:09 +0200

    tests are run through shell script

---
+++ bin/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+#composer --quiet --no-interaction update --optimize-autoloader > /dev/null
+
+[ -x vendor/bin/phpspec ] && vendor/bin/phpspec --no-interaction run -f dot && [ -x vendor/bin/phpunit ] && vendor/bin/phpunit
