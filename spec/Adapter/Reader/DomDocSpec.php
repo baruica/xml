e5c46b6bdd1a24c7b356efc7ad14307753ee77c1
Nelson da Costa <barui.ca@gmail.com>
2016-01-21 15:37:46 +0100

    revert use of puli for now

--- spec/Adapter/Reader/DomDocSpec.php
+++ spec/Adapter/Reader/DomDocSpec.php
@@ -5,20 +5,13 @@ namespace spec\Baruica\Xml\Adapter\Reader;
 use PhpSpec\ObjectBehavior;
 
 use Baruica\Xml\Reader;
-use Puli\Repository\Api\ResourceRepository;
 
 class DomDocSpec extends ObjectBehavior
 {
     public function it_is_initializable_from_a_file_path()
     {
-        $factoryClass = PULI_FACTORY_CLASS;
-        $factory = new $factoryClass();
-
-        /** @var ResourceRepository $repo */
-        $repo = $factory->createRepository();
-
         $this->beConstructedThrough('fromFile', array(
-            $repo->get('/baruica/xml/xml/static_factory_constructor.xml')->getFilesystemPath()
+            __DIR__.'/../../../res/xml/static_factory_constructor.xml'
         ));
 
         $this->shouldHaveType(Reader::class);
